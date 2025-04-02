51de5a2a102cf471bae0d6b91239326fdfaa110b
aakankshab2853 <akankshabhoge05@gmail.com>
2025-04-02 11:52:28 +0530

    add

--- supplier/urls.py
+++ supplier/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import SupplierViewset
 urlpatterns = [
     path('allsupplier',SupplierViewset.as_view({'get':'list'}),) ,
-    path('supplierdetails/<int:pk>/',SupplierViewset.as_view({'get': 'retrieve'}), name='vendor-retrieve'),
-    path('addsupplier/', SupplierViewset.as_view({'post': 'create'}), name='vendor-create'),
-    path('updatesupplier/<int:pk>/',SupplierViewset.as_view({'put': 'update'}), name='vendor-update'),
-    path('deletesupplier/<int:pk>/',SupplierViewset.as_view({'delete': 'destroy'}), name='vendor-delete'),
+    path('supplierdetails/<int:pk>/',SupplierViewset.as_view({'get': 'retrieve'}), name='supplier-retrieve'),
+    path('addsupplier/', SupplierViewset.as_view({'post': 'create'}), name='supplier-create'),
+    path('updatesupplier/<int:pk>/',SupplierViewset.as_view({'put': 'update'}), name='supplier-update'),
+    path('deletesupplier/<int:pk>/',SupplierViewset.as_view({'delete': 'destroy'}), name='supplier-delete'),
 ]
   
   
